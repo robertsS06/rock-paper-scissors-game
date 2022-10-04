6bfe95d82f64d079360fad6b0a976ee3093f45e8
robertsS06 <114050971+robertsS06@users.noreply.github.com>
2022-10-04 18:58:01 +0300

    game.py
    Now we save history of player turns

--- game.py
+++ game.py
@@ -1,25 +1,32 @@
-#UZDEVUMS-Modificē kodu lai spēle atkārtojās
 import random
 turns = ['rock', 'paper', 'scissors']
-
-
+human_turns = []
+computer_turns = []
 
 while(True):
-    human_turn = input("Enter your turn or type exit: ")
+    human_turn = input("Enter your turn, or type exit: ")
     computer_turn = random.choice(turns)
 
     if human_turn == 'exit':
-        print("Goodbye, back to the lobby")
+        print('Thank you for playing! Bye bye')
         break
 
-    print(f"Human:{human_turn} vs. {computer_turn}")
+    human_turns.append(human_turn)
+    computer_turns.append(computer_turn)
+
+    print(f'Human:{human_turn} vs. Computer:{computer_turn}')
     if human_turn == computer_turn:
-        print("Draw!")
-    elif human_turn == "rock" and computer_turn == "scissors":
-        print("Human wins!")
-    elif human_turn == "paper" and computer_turn == "rock":
-        print("Human wins!")
-    elif human_turn == "scissors" and computer_turn == "paper":
-        print("Human wins!")
+        print('Draw!')
+    elif human_turn == 'rock' and computer_turn == 'scissors':
+        print('Human wins!')
+    elif human_turn == 'paper' and computer_turn == 'rock':
+        print('Human wins!')
+    elif human_turn == 'scissors' and computer_turn == 'paper':
+        print('Human wins!')
     else:
-        print("Computer wins!")
+        print('Computer wins!')
+
+
+print(f'You have played {len(human_turns)} times')
+print(human_turns)
+print(computer_turns)
